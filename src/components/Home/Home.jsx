d113f76551bf691a4b023a08b4df1abc08127ed5
FizaShakil <fizashakil25@gmail.com>
2025-05-21 22:02:57 +0500

    Making responsive the home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Heading from '../Reusable-components/Heading'
 import AboutUsHome from './AboutUsHome'
 import Contact from '../Contact/Contact'
 
@@ -7,8 +6,8 @@ const Home = () => {
   return (
     <div>
       <div className=' bg w-full h-[100vh] md:h-[100vh] relative opacity-90'>
-        <div className='flex flex-row'>
-          <div className='text-white headFont2 pt-32 pl-10 text-5xl leading-[50px]'>
+        <div className='lg:flex lg:flex-row'>
+          <div className='text-white headFont2 text-[26px] sm:text-4xl pt-40 text-center lg:text-start lg:pt-32 lg:pl-10 lg:text-5xl lg:leading-[50px]'>
             Escape to Paradise: <br /> Where Sun-Kissed Shores &  <br />Serene Waters Await.
           </div>
         </div>
