8ac15042483b396bc38d9094c76748782307df2f
FizaShakil <fizashakil25@gmail.com>
2025-05-21 22:16:14 +0500

    Making changes in responsiveness

--- src/components/Home/AboutUsHome.jsx
+++ src/components/Home/AboutUsHome.jsx
@@ -7,14 +7,17 @@ const AboutUsHome = () => {
   return (
     <div>
       <Heading h={'About'} />
-      
-      {/* Responsive container: column on small, row on medium+ */}
-      <div className='flex flex-col-reverse md:flex-row justify-around mt-7 items-center'>
-
-        {/* Text Section */}
-        <div className='w-[90%] md:w-[400px] md:mr-[9%] text-center md:text-left mt-6 md:mt-0'>
+      <div className='flex flex-col md:flex-row justify-around items-center mt-7 gap-8'>
+        <div className='hover:shadow-lg hover:shadow-black hover:ease-linear duration-300 max-w-[400px]'>
+          <img
+            src={purpleHotelImage}
+            alt="aboutImage"
+            className='bg-cover w-full rounded-xl'
+          />
+        </div>
+        <div className='max-w-[400px] px-4 md:px-0'>
           <p className='headFont1'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, magnam suscipit voluptatibus dolorem repellat voluptatum hic nisi saepe id nemo officiis maxime dolores, officia quo voluptate quod? Dolore, placeat assumenda. <br /> <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, magnam suscipit voluptatibus dolorem repellat voluptatum hic nisi saepe id nemo officiis maxime dolores, officia quo voluptate quod? Dolore, placeat assumenda. <br /><br />
             Eos unde quisquam corrupti hic non. Rem dicta ad, quidem sit magnam fuga. Porro officiis quod animi quibusdam quaerat atque perferendis, consectetur illo cumque perspiciatis magnam tempore eum iusto numquam?
           </p>
           <Link to='/aboutus'>
@@ -23,18 +26,10 @@ const AboutUsHome = () => {
             </button>
           </Link>
         </div>
-
-        {/* Image Section */}
-        <div className='w-[90%] md:w-[400px] md:ml-[9%] hover:shadow-lg hover:shadow-black hover:ease-linear duration-300 mb-6 md:mb-0'>
-          <img
-            src={purpleHotelImage}
-            alt="aboutImage"
-            className='bg-cover w-full rounded-xl'
-          />
-        </div>
       </div>
     </div>
   );
 };
 
 export default AboutUsHome;
+
